f0160e32caf2b94f0f185e5b9b3f1ba7cf6aae03
Simone <s267562@studenti.polito.it>
2020-12-22 12:17:25 +0100

    Add stepper and implemented step change (speech recognition)

--- src/js/CookingMode.js
+++ src/js/CookingMode.js
@@ -1,24 +1,117 @@
 import { Component, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
+import { MobileStepper, Button } from '@material-ui/core';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
 class CookingMode extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeStep: 0,
+        };
+
+        this.onStepChange = this.onStepChange.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+        this.handleBack = this.handleBack.bind(this);
     }
 
     componentDidMount() {
     }
 
+    onStepChange(command) {
+        if (command === 'next') {
+            this.setState((prevState) => ({ activeStep: parseInt(prevState.activeStep) + 1 }));
+        } else if (command === 'back') {
+            this.setState((prevState) => ({ activeStep: parseInt(prevState.activeStep) - 1 }));
+        } else {
+            /* step number (command) */
+            let stepNumber = undefined;
+            if (isNaN(command)) {
+                switch (command.toLowerCase()) {
+                    case 'zero':
+                        stepNumber = 0;
+                        break;
+                    case 'one':
+                        stepNumber = 1;
+                        break;
+                    case 'two':
+                        stepNumber = 2;
+                        break;
+                    case 'three':
+                        stepNumber = 3;
+                        break;
+                    case 'four':
+                        stepNumber = 4;
+                        break;
+                    case 'five':
+                        stepNumber = 5;
+                        break;
+                    case 'six':
+                        stepNumber = 6;
+                        break;
+                    case 'seven':
+                        stepNumber = 7;
+                        break;
+                    case 'eight':
+                        stepNumber = 8;
+                        break;
+                    case 'nine':
+                        stepNumber = 9;
+                        break;
+                    case 'ten':
+                        stepNumber = 10;
+                        break;
+                    default:
+                        stepNumber = -1;
+                        break;
+
+                }
+            } else {
+                stepNumber = command;
+            }
+            this.setState({ activeStep: parseInt(stepNumber) });
+        }
+    }
+
+    handleNext = () => {
+        this.setState((prevState) => ({ activeStep: parseInt(prevState.activeStep) + 1 }));
+    };
+
+    handleBack = () => {
+        this.setState((prevState) => ({ activeStep: parseInt(prevState.activeStep) - 1 }));
+    };
+
     render() {
         return <>
-            <Rec />
+            <Rec onStepChange={this.onStepChange} />
+            <p>Step # {this.state.activeStep}</p>
+
+            <MobileStepper
+                variant="progress"
+                steps={6}
+                position="static"
+                activeStep={this.state.activeStep}
+                nextButton={
+                    <Button size="small" onClick={this.handleNext} disabled={this.state.activeStep === 5}>
+                        Next
+                        <KeyboardArrowRight />
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={this.handleBack} disabled={this.state.activeStep === 0}>
+                        <KeyboardArrowLeft />
+                        Back
+                    </Button>
+                }
+            />
         </>;
     }
 }
 
-function Rec() { // Rule 2: call hooks in function component
-
+function Rec(props) { // Rule 2: call hooks in function component
     /* Speech recognition */
     const [message, setMessage] = useState('');
     const commands = [
@@ -34,6 +127,14 @@ function Rec() { // Rule 2: call hooks in function component
             command: ['Hello', 'Hi'],
             callback: ({ command }) => setMessage(`Hi there! You said: "${command}"`),
             matchInterim: true
+        },
+        {
+            command: ['next', 'back'],
+            callback: (({ command }) => props.onStepChange(command))
+        },
+        {
+            command: ['Step *'],
+            callback: ((step) => props.onStepChange(step))
         }
     ];
     const { transcript, listening } = useSpeechRecognition({ commands }) // Rule 1: call hooks in top-level
