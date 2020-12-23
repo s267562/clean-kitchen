457443f769fc1fbc648d1cf56c8e097f68ccb334
Simone <s267562@studenti.polito.it>
2020-12-23 01:52:36 +0100

    Fix toolbar and add settings

--- src/App.js
+++ src/App.js
@@ -2,31 +2,74 @@ import { Component } from 'react';
 import './css/App.css';
 import CookingMode from './js/CookingMode';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Home from './js/Home';
 import Tutorial from './js/Tutorial';
 import Search from './js/Search';
 import Recipe from './js/Recipe';
 
+const settingOptions = [
+  'Setting #0',
+  'Setting #1',
+  'Setting #2',
+];
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      anchorEl: null
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
-  componentDidMount() {
-  }
+  handleClick = (event) => {
+    this.setState({
+      anchorEl: event.currentTarget
+    });
+  };
 
-  componentWillUnmount() {
-  }
+  handleClose = (event, index) => {
+    console.log("handleClose - " + settingOptions[index]);
+    this.setState({
+      anchorEl: null
+    });
+  };
 
   render() {
     return (
       <Router>
-        <MyAppbar />
+        <AppBar position="static">
+          <Toolbar >
+            <Typography variant="h6" style={{ flexGrow: 1 }} >
+              Clean Kitchen
+            </Typography>
+            <IconButton edge="end" color="inherit" onClick={this.handleClick}>
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id="setting-menu"
+              anchorEl={this.state.anchorEl}
+              keepMounted
+              open={Boolean(this.state.anchorEl)}
+              onClose={this.handleClose}
+            >
+              {settingOptions.map((option, index) => (
+                <MenuItem
+                  key={option}
+                  onClick={(event) => this.handleClose(event, index)}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route path="/tutorial">
             <Tutorial />
@@ -47,19 +90,6 @@ class App extends Component {
       </Router>
     );
   }
-}
-
-function MyAppbar() {
-  return <AppBar position="static">
-    <Toolbar>
-      <Typography variant="h6" >
-        Clean Kitchen
-      </Typography>
-      <IconButton edge="end" color="inherit" aria-label="menu">
-        <MoreVertIcon />
-      </IconButton>
-    </Toolbar>
-  </AppBar>
 }
 
 export default App;
