61cdcc06fe059da035f0ff7e14cea11aa28175d8
Diego Marino <marinodiego.96@gmail.com>
2020-12-30 14:56:29 +0100

    First draft

--- src/js/Recipe.js
+++ src/js/Recipe.js
@@ -1,4 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { Component } from 'react';
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        flexDirection: 'column',
+        alignItems: 'safe center',
+        padding: 16,
+      },
+      paperMedia: {
+        height: 250,
+        width: '100%',
+        borderRadius: 12,
+      },
+});
 
 class Recipe extends Component {
 
@@ -14,8 +31,24 @@ class Recipe extends Component {
     }
 
     render() {
-        return <p>RECIPE</p>
+        return <>
+                <RecipeOverview />
+            </>
     }
+}
+
+function RecipeOverview() {
+    const classes = useStyles();
+
+    return (
+        <Grid container className={classes.root}>
+            <Grid item>
+              <Paper className={classes.paperMedia}>
+                  <img src='./res/images/carbonara.jpg' alt='Carbonara' style={{height: 'inherit', width: 'inherit', borderRadius: 'inherit',}} />
+              </Paper>
+            </Grid>
+    </Grid>
+    );
 }
 
 export default Recipe;
