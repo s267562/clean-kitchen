9fe6d17dd5f87a43935049de3d8062a6e92359ff
Simone <s267562@studenti.polito.it>
2021-02-10 23:23:09 +0100

    Update search results chips and filter

--- src/js/SearchResults.js
+++ src/js/SearchResults.js
@@ -30,6 +30,12 @@ import CloseIcon from "@material-ui/icons/Close";
 import Grow from "@material-ui/core/Grow";
 import fireAPI from "./fireAPI";
 
+const DEFAULT_FILTERS = {
+  time: [0, 200],
+  difficulty: { easy: false, normal: false, hard: false },
+  cost: { low: false, medium: false, high: false },
+};
+
 const StyledRating = withStyles({
   iconFilled: {
     color: "#c62828",
@@ -57,6 +63,8 @@ const useStyles = makeStyles(() => ({
     minWidth: "150px",
     borderRadius: "8px",
     boxShadow: "0px 0px 16px rgba(34, 35, 58, 0.3)",
+    objectFit: "cover",
+    objectPosition: "center",
   },
 
   dialogDivider: {
@@ -66,15 +74,17 @@ const useStyles = makeStyles(() => ({
 }));
 
 function SearchResults(props) {
-  const [searchResults, setSearchResults] = useState(null);
   const [filterOpen, setFilterOpen] = useState(false);
-  const [filteredRecipes, setFilteredRecipes] = useState(null);
+
+  const [searchResults, setSearchResults] = useState(null); // all the recipes
+  const [filteredRecipes, setFilteredRecipes] = useState(null); // filtered recipes
+  const [filters, setFilters] = useState(DEFAULT_FILTERS);
+  const [chipData, setChipData] = useState([]);
+  const [keyword, setKeyword] = useState("");
+
   const history = useHistory();
 
-  const [chipData, setChipData] = useState([]);
-
   const mountedRef = useRef();
-
   const pendingReq = useRef();
 
   useEffect(() => {
@@ -83,19 +93,22 @@ function SearchResults(props) {
   }, []);
 
   useEffect(() => {
-    let keyword;
+    let kw; // keyword
 
     if (mountedRef.current) {
       /* when component is mounted get the keyword from history */
       mountedRef.current = false;
-      keyword = history.location.search?.replace("?query=", "").replaceAll("%20", " ");
+      kw = history.location.search?.replace("?query=", "").replaceAll("%20", " ");
     } else {
       /* otherwise get the keyword from props (SearchBar) */
-      keyword = props.searchKeyword;
+      kw = props.searchKeyword;
     }
 
-    if (keyword === "") setAllRecipes();
-    else setRecipesByKeyword(keyword);
+    if (kw === "") setAllRecipes();
+    else {
+      setRecipesByKeyword(kw);
+      setKeyword(kw);
+    }
   }, [history, props.searchKeyword]);
 
   const setRecipesByKeyword = (keyword) => {
@@ -127,17 +140,83 @@ function SearchResults(props) {
     });
   };
 
+  useEffect(() => {
+    /* user exit dialog saving a new filters config */
+
+    /* filter the recipes array */
+    setFilteredRecipes(
+      searchResults?.filter((recipe) => {
+        return checkDuration(recipe, filters) && checkDifficulty(recipe, filters) && checkCost(recipe, filters);
+      })
+    );
+
+    /* update chipData */
+    let chipDataArray = [];
+    if (filters.time[0] !== 0 || filters.time[1] !== 200)
+      chipDataArray.push({
+        key: 0,
+        label: `${filters.time[0]} - ${filters.time[1] === 200 ? "200+" : filters.time[1]} min`,
+      });
+    if (filters.cost.low) chipDataArray.push({ key: 1, label: "Low" });
+    if (filters.cost.medium) chipDataArray.push({ key: 2, label: "Medium" });
+    if (filters.cost.high) chipDataArray.push({ key: 3, label: "High" });
+    if (filters.difficulty.easy) chipDataArray.push({ key: 4, label: "Easy" });
+    if (filters.difficulty.normal) chipDataArray.push({ key: 5, label: "Normal" });
+    if (filters.difficulty.hard) chipDataArray.push({ key: 6, label: "Hard" });
+    setChipData(chipDataArray);
+  }, [filters]);
+
+  const handleChipDelete = (chipToDelete) => () => {
+    let newFilters;
+    switch (chipToDelete.key) {
+      case 0:
+        newFilters = { ...filters, time: DEFAULT_FILTERS.time };
+        break;
+      case 1:
+        newFilters = { ...filters, cost: { ...filters.cost, low: DEFAULT_FILTERS.cost.low } };
+        break;
+      case 2:
+        newFilters = { ...filters, cost: { ...filters.cost, medium: DEFAULT_FILTERS.cost.medium } };
+        break;
+      case 3:
+        newFilters = { ...filters, cost: { ...filters.cost, high: DEFAULT_FILTERS.cost.high } };
+        break;
+      case 4:
+        newFilters = { ...filters, difficulty: { ...filters.difficulty, easy: DEFAULT_FILTERS.cost.easy } };
+        break;
+      case 5:
+        newFilters = { ...filters, difficulty: { ...filters.difficulty, normal: DEFAULT_FILTERS.cost.normal } };
+        break;
+      case 6:
+        newFilters = { ...filters, difficulty: { ...filters.difficulty, hard: DEFAULT_FILTERS.cost.hard } };
+        break;
+      default:
+        break;
+    }
+    setFilters(newFilters);
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+  };
+
   const handleFilterClick = () => {
     setFilterOpen(true);
   };
 
-  if (filteredRecipes === null) {
+  const handleResetFilters = () => {
+    setFilters(DEFAULT_FILTERS);
+  };
+
+  if (
+    filteredRecipes === null ||
+    filteredRecipes === undefined ||
+    searchResults === null ||
+    searchResults === undefined
+  ) {
     return <LoadingComponent />;
-  } else if (filteredRecipes.length > 0) {
+  } else if (searchResults.length > 0) {
     return (
       <>
-        <Box display='flex' flexDirection='column' style={{ marginBottom: "8px" }}>
-          <Button style={{ padding: "16px", margin: "auto" }} startIcon={<TuneIcon />} onClick={handleFilterClick}>
+        <Box display='flex' flexDirection='column' style={{ marginBottom: "8px", height: "calc(100% - 128px)" }}>
+          <Button style={{ padding: "16px", margin: "0px auto" }} startIcon={<TuneIcon />} onClick={handleFilterClick}>
             Filter
           </Button>
           <Box
@@ -166,9 +245,7 @@ function SearchResults(props) {
                     variant='outlined'
                     size='small'
                     label={data.label}
-                    onClick={() => {
-                      setFilterOpen(true);
-                    }}
+                    onDelete={handleChipDelete(data)}
                     style={{ padding: "4px", margin: "4px" }}
                     icon={icon}
                   />
@@ -176,17 +253,38 @@ function SearchResults(props) {
               );
             })}
           </Box>
-          {filteredRecipes.map((recipe) => (
-            <Recipe key={recipe.id} recipe={recipe} searchKeyword={props.searchKeyword} />
-          ))}
+          {filteredRecipes.length > 0 ? (
+            filteredRecipes.map((recipe) => (
+              <Recipe key={recipe.id} recipe={recipe} searchKeyword={props.searchKeyword} />
+            ))
+          ) : (
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                marginRight: "16px",
+                marginLeft: "16px",
+              }}
+            >
+              <Typography variant='h5'>No results match.</Typography>
+              <Typography style={{ color: "#757575", marginTop: "16px", textAlign: "center" }}>
+                The active filters are hiding all the "{keyword}" recipes.
+              </Typography>
+              <Button style={{ padding: "16px", margin: "0px auto" }} color='secondary' onClick={handleResetFilters}>
+                Clear Filters
+              </Button>
+            </div>
+          )}
         </Box>
         <FilterDialog
           open={filterOpen}
           setOpen={setFilterOpen}
           recipes={searchResults}
-          setResult={setFilteredRecipes}
-          chipData={chipData}
-          setChipData={setChipData}
+          savedFilters={filters}
+          saveFilters={setFilters}
         />
       </>
     );
@@ -206,7 +304,7 @@ function SearchResults(props) {
         <img src='/res/images/noresults.png' alt='no results' />
         <Typography variant='h4'>No results found</Typography>
         <Typography style={{ color: "#757575", marginTop: "16px", textAlign: "center" }}>
-          Try adjusting your search or filter to find what you're searching for.
+          Try adjusting your search to find what you're searching for.
         </Typography>
       </div>
     );
@@ -236,7 +334,7 @@ function Recipe(props) {
 
   return (
     <Grow in={true}>
-      <Card className={styles.card} onClick={onCardClick}>
+      <Card className={styles.card} onClick={onCardClick} style={{ minHeight: "125px" }}>
         <CardMedia className={styles.media} image={recipe.overviewImg} />
         <Grid
           container
@@ -244,8 +342,8 @@ function Recipe(props) {
           justify='space-between'
           alignItems='stretch'
           style={{
+            minHeight: "100%",
             overflow: "hidden",
-            height: "100%",
             marginLeft: "16px",
             paddingTop: "8px",
             paddingBottom: "8px",
@@ -278,7 +376,7 @@ function Recipe(props) {
             </Box>
             <Box display='flex' flexDirection='row'>
               <TimerIcon fontSize='small' style={{ marginRight: "8px" }} />
-              <Typography style={{ fontSize: "0.9em" }}> {recipe.duration} min </Typography>
+              <Typography style={{ fontSize: "0.9em" }}> {recipe.duration}' </Typography>
               <Divider orientation='vertical' style={{ marginLeft: "8px", marginRight: "8px", height: "21px" }} />
               <StyledRating
                 name='cost-rating'
@@ -330,117 +428,61 @@ const CustomSlider = withStyles({
 
 function FilterDialog(props) {
   const classes = useStyles();
-  const { open, setOpen, setResult, chipData, setChipData } = props;
-  const [filteredRecipes, setFilteredRecipes] = useState(props.recipes);
-  const [time, setTime] = useState([0, 200]);
-  const [difficulty, setDifficulty] = useState({ easy: false, normal: false, hard: false });
-  const [cost, setCost] = useState({ low: false, medium: false, high: false });
-
-  const [savedTime, setSavedTime] = useState([0, 200]);
-  const [savedDifficulty, setSavedDifficulty] = useState({ easy: false, normal: false, hard: false });
-  const [savedCost, setSavedCost] = useState({ low: false, medium: false, high: false });
+  const { open, setOpen, savedFilters, saveFilters, recipes } = props;
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+  const [currentFilters, setCurrentFilters] = useState(DEFAULT_FILTERS);
 
   const handleClose = () => {
     setOpen(false);
-    resetSavedFilters();
-  };
-
-  const resetSavedFilters = () => {
-    setTime(savedTime);
-    setDifficulty(savedDifficulty);
-    setCost(savedCost);
+    setCurrentFilters(savedFilters); // restore saved filters
   };
 
   const handleReset = () => {
-    setTime([0, 200]);
-    setDifficulty({ easy: false, normal: false, hard: false });
-    setCost({ low: false, medium: false, high: false });
+    setCurrentFilters(DEFAULT_FILTERS);
   };
 
   const handleApply = () => {
-    setResult(filteredRecipes);
-    saveFilters();
-    let chipDataArray = [];
-    if (time[0] !== 0 || time[1] !== 200)
-      chipDataArray.push({ key: 0, label: `${time[0]} - ${time[1] === 200 ? "200+" : time[1]} min` });
-    if (cost.low) chipDataArray.push({ key: 1, label: "Low" });
-    if (cost.medium) chipDataArray.push({ key: 2, label: "Medium" });
-    if (cost.high) chipDataArray.push({ key: 3, label: "High" });
-    if (difficulty.easy) chipDataArray.push({ key: 4, label: "Easy" });
-    if (difficulty.normal) chipDataArray.push({ key: 5, label: "Normal" });
-    if (difficulty.hard) chipDataArray.push({ key: 6, label: "Hard" });
-
-    setChipData(chipDataArray);
+    saveFilters(currentFilters); // save current filters
     setOpen(false);
   };
 
-  const saveFilters = () => {
-    setSavedTime(time);
-    setSavedDifficulty(difficulty);
-    setSavedCost(cost);
-  };
-
   const handleTimeChange = (event, newTime) => {
-    setTime(newTime);
+    setCurrentFilters({ ...currentFilters, time: newTime });
   };
 
   const handleDifficultyChange = (event) => {
-    setDifficulty({ ...difficulty, [event.target.name]: event.target.checked });
+    setCurrentFilters({
+      ...currentFilters,
+      difficulty: { ...currentFilters.difficulty, [event.target.name]: event.target.checked },
+    });
   };
 
   const handleCostChange = (event) => {
-    setCost({ ...cost, [event.target.name]: event.target.checked });
+    setCurrentFilters({
+      ...currentFilters,
+      cost: { ...currentFilters.cost, [event.target.name]: event.target.checked },
+    });
   };
 
   useEffect(() => {
-    setFilteredRecipes(props.recipes);
-  }, props.recipes);
+    setFilteredRecipes(recipes);
+  }, [recipes]);
+
+  useEffect(() => {
+    setCurrentFilters(savedFilters);
+  }, [savedFilters]);
 
   useEffect(() => {
     setFilteredRecipes(
-      props.recipes.filter((recipe) => {
-        return checkDuration(recipe) && checkDifficulty(recipe) && checkCost(recipe);
+      recipes.filter((recipe) => {
+        return (
+          checkDuration(recipe, currentFilters) &&
+          checkDifficulty(recipe, currentFilters) &&
+          checkCost(recipe, currentFilters)
+        );
       })
     );
-  }, [time, difficulty, cost]);
-
-  function checkDuration(recipe) {
-    const MAX_TIME = 200;
-    if (time[1] === MAX_TIME) {
-      /* if the user select MAX_TIME as time[1] = max filter time check only the lower bound */
-      return recipe.duration >= time[0];
-    } else {
-      return recipe.duration >= time[0] && recipe.duration <= time[1];
-    }
-  }
-  function checkDifficulty(recipe) {
-    if (!difficulty.easy && !difficulty.normal && !difficulty.hard) return true;
-
-    switch (recipe.difficulty.toLowerCase()) {
-      case "easy":
-        return difficulty.easy;
-      case "normal":
-        return difficulty.normal;
-      case "hard":
-        return difficulty.hard;
-      default:
-        return false;
-    }
-  }
-  function checkCost(recipe) {
-    if (!cost.low && !cost.medium && !cost.high) return true;
-
-    switch (recipe.cost) {
-      case 1:
-        return cost.low;
-      case 2:
-        return cost.medium;
-      case 3:
-        return cost.high;
-      default:
-        return false;
-    }
-  }
+  }, [currentFilters]);
 
   return (
     <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
@@ -475,10 +517,19 @@ function FilterDialog(props) {
             Time range
           </Typography>
           <Typography variant='overline' style={{ color: "#757575" }}>
-            {`${time[0]} - ${time[1]}${(time[1] === 200 && "+") || ""} min.`}
+            {`${currentFilters.time[0]} - ${currentFilters.time[1]}${
+              (currentFilters.time[1] === 200 && "+") || ""
+            } min.`}
           </Typography>
           <div style={{ paddingLeft: "12px", paddingRight: "12px" }}>
-            <CustomSlider value={time} onChange={handleTimeChange} color='secondary' min={0} step={5} max={200} />
+            <CustomSlider
+              value={currentFilters.time}
+              onChange={handleTimeChange}
+              color='secondary'
+              min={0}
+              step={5}
+              max={200}
+            />
           </div>
           <Divider className={classes.dialogDivider} />
 
@@ -488,15 +539,25 @@ function FilterDialog(props) {
           <FormControl component='fieldset'>
             <FormGroup>
               <FormControlLabel
-                control={<Checkbox checked={difficulty.easy} onChange={handleDifficultyChange} name='easy' />}
+                control={
+                  <Checkbox checked={currentFilters.difficulty.easy} onChange={handleDifficultyChange} name='easy' />
+                }
                 label='Easy'
               />
               <FormControlLabel
-                control={<Checkbox checked={difficulty.normal} onChange={handleDifficultyChange} name='normal' />}
+                control={
+                  <Checkbox
+                    checked={currentFilters.difficulty.normal}
+                    onChange={handleDifficultyChange}
+                    name='normal'
+                  />
+                }
                 label='Normal'
               />
               <FormControlLabel
-                control={<Checkbox checked={difficulty.hard} onChange={handleDifficultyChange} name='hard' />}
+                control={
+                  <Checkbox checked={currentFilters.difficulty.hard} onChange={handleDifficultyChange} name='hard' />
+                }
                 label='Hard'
               />
             </FormGroup>
@@ -511,15 +572,17 @@ function FilterDialog(props) {
               <Box display='flex'>
                 <Box display='flex' flexDirection='column'>
                   <FormControlLabel
-                    control={<Checkbox checked={cost.low} onChange={handleCostChange} name='low' />}
+                    control={<Checkbox checked={currentFilters.cost.low} onChange={handleCostChange} name='low' />}
                     label='Low'
                   />
                   <FormControlLabel
-                    control={<Checkbox checked={cost.medium} onChange={handleCostChange} name='medium' />}
+                    control={
+                      <Checkbox checked={currentFilters.cost.medium} onChange={handleCostChange} name='medium' />
+                    }
                     label='Medium'
                   />
                   <FormControlLabel
-                    control={<Checkbox checked={cost.high} onChange={handleCostChange} name='high' />}
+                    control={<Checkbox checked={currentFilters.cost.high} onChange={handleCostChange} name='high' />}
                     label='High'
                   />
                 </Box>
@@ -579,5 +642,43 @@ function FilterDialog(props) {
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction='up' ref={ref} {...props} />;
 });
+
+const checkDuration = (recipe, filters) => {
+  const MAX_TIME = 200;
+  if (filters.time[1] === MAX_TIME) {
+    /* if the user select MAX_TIME as time[1] = max filter time check only the lower bound */
+    return recipe.duration >= filters.time[0];
+  } else {
+    return recipe.duration >= filters.time[0] && recipe.duration <= filters.time[1];
+  }
+};
+const checkDifficulty = (recipe, filters) => {
+  if (!filters.difficulty.easy && !filters.difficulty.normal && !filters.difficulty.hard) return true;
+
+  switch (recipe.difficulty.toLowerCase()) {
+    case "easy":
+      return filters.difficulty.easy;
+    case "normal":
+      return filters.difficulty.normal;
+    case "hard":
+      return filters.difficulty.hard;
+    default:
+      return false;
+  }
+};
+const checkCost = (recipe, filters) => {
+  if (!filters.cost.low && !filters.cost.medium && !filters.cost.high) return true;
+
+  switch (recipe.cost) {
+    case 1:
+      return filters.cost.low;
+    case 2:
+      return filters.cost.medium;
+    case 3:
+      return filters.cost.high;
+    default:
+      return false;
+  }
+};
 
 export default SearchResults;
